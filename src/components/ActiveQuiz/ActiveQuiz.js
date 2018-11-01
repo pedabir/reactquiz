732ec8f59a822008c1d3c871b97df225d782893d
Igor <i.makhonkov@gmail.com>
2018-11-01 17:13:32 +0200

    push project (lesson 6 complete)

---
+++ src/components/ActiveQuiz/ActiveQuiz.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import classes from './ActiveQuiz.css'
+import AnswerList from './AnswersList/AnswersList'
+
+const ActiveQuiz = props => (
+    <div className={classes.ActiveQuiz}>
+        <p className={classes.Question}>
+            <span>
+            <strong>{props.answerNumber} </strong>&nbsp;
+                {props.question}
+            </span>
+            <small>{props.answerNumber} из {props.quizLength}</small>
+        </p>
+
+        <AnswerList 
+            state={props.state}
+            answers={props.answers}
+            onAnswerClick={props.onAnswerClick}
+        />
+    </div>
+)
+
+export default ActiveQuiz
